25028338d6fbcd5b64689d00cb99282f187adbc3
MOHAMED AYMANE ELACHIRI <satayman41@gmail.com>
2025-09-30 13:57:29 +0100

    adding

--- init/hellocounter/app/build.gradle.kts
+++ init/hellocounter/app/build.gradle.kts
@@ -56,4 +56,11 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.material:material-icons-extended")
 }
--- init/hellocounter/app/src/main/java/com/example/hellocounter/MainActivity.kt
+++ init/hellocounter/app/src/main/java/com/example/hellocounter/MainActivity.kt
@@ -11,39 +11,17 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-
-
+import com.example.hellocounter.ui.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { App() }
+        setContent { MaterialTheme { MainScreen() } }
     }
 }
 
+@Preview(showBackground = true, widthDp = 360)
 @Composable
-fun App() {
-    MaterialTheme {
-        Greeting()
-    }
-}
-
-@Composable
-fun Greeting() {
-    var count by remember { mutableStateOf(0) }
-
-    // Box = conteneur qui permet d'aligner son contenu
-    Box(
-        modifier = Modifier.fillMaxSize(),              // occupe tout l'écran
-        contentAlignment = Alignment.Center             // centre le contenu
-    ) {
-        Button(onClick = { count++ }) {
-            Text("Hello, count = $count")
-        }
-    }
-}
-@Preview(showBackground = true)
-@Composable
-fun PreviewApp() {
-    App()
+fun PreviewMainScreen() {
+    MaterialTheme { MainScreen() }
 }
